cf3401a4dbb978b78e3b6413638c507a5baaca15
asifaqeel50 <asifaqeel50@gmail.com>
2024-07-21 01:32:31 +0500

    initial commit

---
+++ Basic Code MongoDB/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import bookRouter from "./routes/book.js";
+
+const app = express();
+app.use(express.json());
+
+const connection = mongoose.connection;
+
+connection.once("connected", () => console.log("Database Connected ~"));
+connection.on("error", (error) => console.log("Database Error: ", error));
+
+mongoose.connect("mongodb://127.0.0.1:27017/my_first_data_base", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use("/book", bookRouter);
+
+app.listen(9999);
